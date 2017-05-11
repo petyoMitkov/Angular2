6e5a9e0d8c15d0410c059a9514a2e52583683539
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-05-11 23:17:25 +0300

    Creat My Examples and Added MyComponent example

---
+++ @My Examples/01_Variables, MyComponent/src/app/my.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "my-component",
+    templateUrl: './my.component.html',
+    styleUrls: ['./my.component.css']
+})
+
+export class MyComponent {
+    
+}
