421b7da9b86347784b494657886e46db832ddbf8
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-05-14 22:21:33 +0300

    jQuery , (click), (mouseenter)

---
+++ @My Examples/03_Data dinding, click, mouseenter & jQuery/src/app/example-pipes.component/my-filter.pape.ts
@@ -0,0 +1,12 @@
+import { ExamplePipesComponent } from './example-pipes.component';
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'myFilter'
+})
+export class MyFilter implements PipeTransform {
+    public transform(arr: any[]): any {
+        return arr.filter(e => e.id > 11);
+        //console.log(arr[1].name);
+    }
+}
